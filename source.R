b41da6c409719b0db03abbab278a7cf98a2d2bdf
Ethan Hurwitz <hurwitz.ethan@gmail.com>
2022-06-10 11:44:06 -0700

    fix out of date package and update run schedule

---
+++ source.R
@@ -0,0 +1,55 @@
+here::here()
+
+# All values that you include MUST be wrapped in single quotes!
+
+# the link to your googlesheets page
+
+googlesheetLink = 'https://docs.google.com/spreadsheets/d/1-ArqgmImgbQhoZYYunBLxQlk9dO5bVUIcjKfZl-czdE/edit?usp=sharing'
+
+# Full name that will appear at the top of your CV
+
+fullName = 'Ethan Hurwitz'
+
+# Institution / Address that will be right under your name. If you want any line breaks,
+  # include a <br>.
+
+institution = 'University of California, San Diego<br>Department of Psychology'
+
+# Socials / Links
+  # Use TRUE or FALSE (no quotes) to denote if you want shown
+
+Email.show = TRUE
+Email = 'ehurwitz@ucsd.edu'
+Website.show = TRUE
+Website = 'www.EthanHurwitz.com'
+Github.show = TRUE
+Github = 'EthanHurwitz'
+OSF.show = FALSE
+OSF = 'phyeb'
+Scholar.show = FALSE
+Scholar = 'https://scholar.google.com/citations?user=hciXZQQAAAAJ'
+
+# CV body
+  # Use TRUE or FALSE (no quotes) to denote if you want a section shown
+
+Education.show = TRUE
+GrantsFunding.show = TRUE
+AwardsHonors.show = TRUE
+Publications.show = TRUE
+  Journals.pub.show = TRUE
+  Proceedings.pub.show = TRUE
+Conference.show = TRUE
+  Conference.oral.show = TRUE
+  Conference.poster.show = TRUE
+Talks.show = TRUE
+Teaching.show = TRUE
+  Teaching.instructor.show = TRUE
+  Teaching.TA.show = TRUE
+  Teaching.seniorTA.show = TRUE
+SOP.show = TRUE
+  SOP.reviewer.show = TRUE
+ClinicalExp.show = TRUE
+  ClinicalExp.RPC.show = TRUE
+  
+  
+  
--- sync_w_gsheets.R
+++ sync_w_gsheets.R
@@ -1,14 +1,18 @@
 library(tidyverse)
 library(googlesheets4)
 
+source(here::here("source.R"))
+
 gs4_deauth()
 
 sheets <- c("cv_entries", 
             "publications")
 
 store_sheet_in_csv <- function(sheet_name){
-  read_sheet("https://docs.google.com/spreadsheets/d/1-ArqgmImgbQhoZYYunBLxQlk9dO5bVUIcjKfZl-czdE/edit?usp=sharing",
-             sheet = sheet_name, col_types = c("c")) %>% 
+  # read_sheet("https://docs.google.com/spreadsheets/d/1-ArqgmImgbQhoZYYunBLxQlk9dO5bVUIcjKfZl-czdE/edit?usp=sharing",
+  #            sheet = sheet_name, col_types = c("c")) %>% 
+  read_sheet(glue(googlesheetLink),
+             sheet = sheet_name, col_types = c("c")) %>%   
     write_csv(here::here(str_c("_content/", sheet_name, ".csv")))
 }
 
